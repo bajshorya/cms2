80bdfc9b04f8396d96d4f2f331d3d4b130224cac
SHORYA BAJ <jain.shorya11@gmail.com>
2024-08-24 22:06:29 +0530

    installed and set up prisma

--- pj1/app/home/page.tsx
+++ pj1/app/home/page.tsx
@@ -1,27 +1,6 @@
 import { Button } from "@/components/ui/button";
-import HomeSelection from "@/components/HomeSelection";
 import React from "react";
 const page = () => {
-  return (
-    <div className="h-screen flex items-center">
-      <div className="w-1/2 flex justify-center items-center ">
-        <HomeSelection
-          toPage="https://100xdevs.com/"
-          label=" Enroll Now and start your coding DEV Journey! "
-          to="100xDevs"
-        />
-      </div>
-
-      <div className="w-0.5 bg-gray-500 h-full"></div>
-
-      <div className="w-1/2 flex justify-center items-center ">
-        <HomeSelection
-          toPage="/solutions"
-          label="Click Here To see the solutions "
-          to="Solutions"
-        />
-      </div>
-    </div>
-  );
+  return <div className="h-screen flex items-center"></div>;
 };
 export default page;
